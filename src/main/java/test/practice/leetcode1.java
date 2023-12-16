c539eaca7664851ee985cb80bc79f28834e2eac4
Jaad Quraan <quraanjaad@gmail.com>
2023-12-16 16:57:41 -0500

    vercel tasks

--- src/main/java/test/practice/leetcode1.java
+++ src/main/java/test/practice/leetcode1.java
@@ -6,8 +6,9 @@ import test.base.TestBase;
 
 public class leetcode1 extends TestBase {
 
-    @Test
-    public void test(){
-        driver.findElement(By)
-    }
+//    @Test
+//    public void test(){
+//        driver.findElement(By)
+//    }
+//}
 }
